bc5b1f61d78306cdb77be02313348d3026e2c501
Ilya Lykov <ilya.lykov.program@gmail.com>
2023-04-11 13:14:33 +0400

    add: add route of url's

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 import express from "express";
 
-const host = "127.0.0.1";
 const port = 8080;
 const app = express();
 
-
-app.get("/hello", (req, resp) => {
-	resp.send("Hello World!!!");
+app.all("/hello", (req, resp, next) => {
+	console.log("All");
+	next();
 });
 
+const cb = (req, resp, next) => {
+	console.log("CB");
+	next();
+};
+
+// app.route("/user")
+// 	.get("/hello", cb, (req, resp) => {
+// 		resp.send("Hello World!!!");
+// 	}).post("/hello", (req, resp) => {
+// 		resp.send("Hello World by POST");
+// 	});
+
+app.route('/user')
+	.get((req, res) => {
+		res.send("Hello World by GET");
+	})
+	.post((req, resp) => {
+		resp.send("Hello World by POST");
+	});
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
