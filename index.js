41d9f65f2e66c4b0d86a81860720d83eb5c6a8eb
Ilya Lykov <ilya.lykov.program@gmail.com>
2023-04-11 12:24:09 +0400

    refactor: change http lib to express

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-import http from "http";
+import express from "express";
 
 const host = "127.0.0.1";
 const port = 8080;
-
-const server = http.createServer((req, resp) => {
-	switch (req.method) {
-		case "GET":
-			switch (req.url) {
-				case "/hello":
-					resp.statusCode = 200;
-					resp.setHeader("Content-Type", "text/html");
-					resp.end("<h1>Hello World!!!</h1>");
-					break;
-			}
-			break;
-	}
+const app = express();
 
 
+app.get("/hello", (req, resp) => {
+	resp.send("Hello World!!!");
 });
 
-server.listen(port, host, () => {
-	console.log(`Сервер запущен на ${host}:${port}`);
+
+app.listen(port, () => {
+	console.log(`Сервер запущен на http://localhost:${port}`);
 });
