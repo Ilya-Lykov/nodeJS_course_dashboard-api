a9abd49a118ca53d4938dba3a8c407cb2d891f5a
Lykov Ilya <ilya.lykov.program@gmail.com>
2023-04-12 16:34:19 +0400

    refactor: migrate to TS from JS

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { usersRouter } from "./users/users.js";
 
 const port = 8080;
@@ -15,12 +15,11 @@ app.get("/hello", (req, resp) => {
 
 app.use("/users", usersRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(err.message);
 	res.status(500).send(err.message);
 });
 
-
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
-}); 
+});
