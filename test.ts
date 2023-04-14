c9153be341c823452be50882cfb3fa61f454cd78
Lykov Ilya <ilya.lykov.program@gmail.com>
2023-04-14 15:45:16 +0400

    add: work types in ts

---
+++ test.ts
@@ -0,0 +1,18 @@
+let a: number = 5;
+let b: string = "4";
+
+let c: number = a + Number(b);
+
+let names: string[] = ["qweqw", "qweqwe"];
+
+let tup: [number, string] = [2, "asdas"];
+
+let e: any = 3;
+
+function greet(name: string): string {
+	return name + "HI";
+}
+
+names.map((x: string) => x);
+
+function coord(coord: { lat: number; long: number });
