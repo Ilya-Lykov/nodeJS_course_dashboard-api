f83c2f007e8780df44566b2bcb7316518b47072e
Ilya Lykov <ilya.lykov.program@gmail.com>
2023-04-16 22:17:34 +0400

    add:work with classes

--- test.js
+++ test.js
@@ -1,13 +1,36 @@
-var universalID = 5;
-console.log(universalID);
-printId(universalID);
-universalID = "sdf";
-printId(universalID);
-function printId(id) {
-    if (typeof id == "number") {
-        console.log("Number:" + id);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Coord = /** @class */ (function () {
+    function Coord(lat, long) {
+        this.message = "1";
+        this.lat = lat;
+        this.long = long;
+        console.log(this.message);
     }
-    else {
-        console.log(id.toUpperCase());
+    return Coord;
+}());
+var point = new Coord(0, 1);
+var MapLocation = /** @class */ (function (_super) {
+    __extends(MapLocation, _super);
+    function MapLocation(lat, long, name) {
+        var _this = _super.call(this, lat, long) || this;
+        _this.message = "2";
+        _this.name = name;
+        console.log(_this.message);
+        return _this;
     }
-}
+    return MapLocation;
+}(Coord));
--- test.ts
+++ test.ts
@@ -1,19 +1,94 @@
-interface HasLegth {
-	length: number;
+class Coord {
+	message = "1";
+	lat: number;
+	long: number;
+
+	computeDistance(newLat: number, newLong: number) {
+		return 0;
+	}
+
+	protected test() {
+		if (this.lat > 0) {
+		}
+	}
+
+	constructor(lat: number, long: number) {
+		this.lat = lat;
+		this.long = long;
+		console.log(this.message);
+	}
 }
 
-function log<T extends HasLegth, K>(obj: T, arr: K[]): T {
-	obj.length;
-	console.log(obj);
-	return obj;
+class MapLocation extends Coord {
+	message = "2";
+
+	_name: string;
+
+	get name() {
+		return this._name;
+	}
+	set name(s: string) {
+		this._name = s;
+	}
+
+	override computeDistance(newLat: number, newLong: number): number {
+		console.log(this.name);
+		return 1;
+	}
+
+	constructor(lat: number, long: number, name: string) {
+		super(lat, long);
+		this.name = name;
+		console.log(this.message);
+	}
+}
+const point = new MapLocation(0, 1, "NY");
+
+interface LoggerService {
+	log: (s: string) => void;
 }
 
-interface IUser {
+class Logger implements LoggerService {
+	log(s: string) {
+		console.log(s);
+	}
+}
+
+const l = new Logger();
+l.log("Hello");
+
+class MyClass<T> {
+	a: T;
+}
+
+const b = new MyClass<String>();
+b.a;
+
+abstract class Base {
+	print(s: string) {
+		console.log(s);
+	}
+
+	abstract error(s: string): void;
+}
+
+class BaseChild extends Base {
+	override error(s: string): void {
+		throw new Error(s);
+	}
+
+	override print(s: string): void {
+		console.log(s);
+	}
+}
+
+class Animal {
 	name: string;
-	age?: number;
-	bid: <T>(sum: T) => boolean;
 }
 
-function bid<T>(sum: T): boolean {
-	return true;
+class Dog {
+	name: string;
+	tail: boolean;
 }
+
+const puppy: Animal = new Dog();
