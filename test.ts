6d6751a2290190d9520a4840abf2235a6dc673a9
Ilya Lykov <ilya.lykov.program@gmail.com>
2023-04-16 19:57:15 +0400

    add: work with enums

--- test.ts
+++ test.ts
@@ -1,37 +1,18 @@
-// const a = "asdasd";
+enum Direction {
+	Left,
+	Right,
+}
 
-let b: "hi" = "hi";
-
-type direction = "left" | "right";
-
-function moveDog(direction: direction): -1 | 0 | 1 {
+function move(direction: Direction) {
 	switch (direction) {
-		case "left":
+		case Direction.Left:
 			return -1;
-		case "right":
+		case Direction.Right:
 			return 1;
-		default:
-			return 0;
 	}
 }
-
-interface IConnection {
-	host: string;
-	port: number;
+const enum Direction2 {
+	Up,
+	Down,
 }
-
-// function connect(connection: IConnection | "default") {}
-
-// connect("default");
-
-const connection = {
-	host: "localhost",
-	protocol: "https" as "https",
-};
-
-let a: any = 5;
-let c: number = a as number;
-
-function connect(host: string, protocol: "http" | "https") {}
-
-connect(connection.host, connection.protocol);
+let myDirection = Direction2.Up;
