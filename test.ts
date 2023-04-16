bc522689827431cce517fbd8dc174aa732f525a6
Ilya Lykov <ilya.lykov.program@gmail.com>
2023-04-16 19:48:48 +0400

    add: work with literals

--- test.ts
+++ test.ts
@@ -1,22 +1,37 @@
-type coord = { lat: number; long: number };
+// const a = "asdasd";
 
-interface ICoord {
-	lat: number;
-	long: number;
+let b: "hi" = "hi";
+
+type direction = "left" | "right";
+
+function moveDog(direction: direction): -1 | 0 | 1 {
+	switch (direction) {
+		case "left":
+			return -1;
+		case "right":
+			return 1;
+		default:
+			return 0;
+	}
 }
 
-type ID = number | string;
-
-function compute(coord: ICoord) {}
-
-interface Dog {
-	name: string;
-}
-interface Dog {
-	tail: boolean;
+interface IConnection {
+	host: string;
+	port: number;
 }
 
-const dog: Dog = {
-	name: "fsdf",
-	tail: true,
+// function connect(connection: IConnection | "default") {}
+
+// connect("default");
+
+const connection = {
+	host: "localhost",
+	protocol: "https" as "https",
 };
+
+let a: any = 5;
+let c: number = a as number;
+
+function connect(host: string, protocol: "http" | "https") {}
+
+connect(connection.host, connection.protocol);
